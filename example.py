bec6e2bafcce43d24a0b973ce12d3860ce0ec40a
dpjrodrigues <dpjrodrigues@gmail.com>
2019-02-27 21:26:05 +0000

    first commit

---
+++ example.py
@@ -0,0 +1,31 @@
+import asyncio
+import aiohttp
+
+from pyCTT import Items, CTT
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        items = await Items.get(session,'LM417779776CN,LM417779766CN,LX600717217NL,LX581418164NL,LX581455065NL,LX581431850NL')
+
+        print("All Items:")
+        for item in await items.full_list():
+            print(item)
+
+        print('Total Number of Items:',await items.number_of_items())
+
+        print('Total Number of Items Delivered:',await items.number_of_items_delivered())
+
+        print('Total Number of Items not Delivered:',await items.number_of_items_not_delivered())
+
+        print("Delivered Items:")
+        for item in await items.delivered_list():
+            print(item)
+
+        print("Not delivered Items:")
+        for item in await items.not_delivered_list():
+            print(item)
+
+
+asyncio.get_event_loop().run_until_complete(main())
+
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+long_description = open('README.md').read()
+
+setup(
+    name='pyCTT',
+    version='1.0.0',
+    license='MIT License',
+    url='https://github.com/dpjrodrigues/pyCTT',
+    author='Diogo Rodrigues',
+    author_email='dpjrodrigues@gmail.com',
+    description='Python library to retrieve package information from CTT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['pyCTT'],
+    zip_safe=True,
+    platforms='any',
+    install_requires=[
+        'aiohttp',
+        'lxml'
+      ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
